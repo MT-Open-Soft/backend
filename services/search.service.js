fd951dc15a816dd57c52d1e58926b76b2d76c40f
soumili202 <soumilirupsa2020@gmail.com>
2024-03-18 01:51:39 +0530

    Added Changes

--- services/search.service.js
+++ services/search.service.js
@@ -3,82 +3,81 @@ const {movieModel} = require("../models");
 const suggest = async(query) => {
     const searchStage = {
       index: "sample_mflix-movies-static",
-      /*weights: {
-        title: 10,
-        plot: 5,
-        fullplot: 2,
-        genres: 5,
-        cast: 5,
-        directors: 5
-      },*/
+      
       compound: {
         should: [{
           autocomplete: {
             query,
             path: "title",
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
+            tokenOrder: "sequential",
+            score: {
+              boost: {
+                value: 7
+              }
             }
+            
           }
         },
         {
           autocomplete: {
             query,
             path: "plot",
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
+            tokenOrder: "sequential",
+            score: {
+              boost: {
+                value: 3
+              }
             }
+            
           }
         },
-        {
-          autocomplete:{
-            query,
-            path: "fullplot",
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
-            }
-          }
-        },
+        
         {
           autocomplete:{
             query,
             path: "genres",
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
+            score: {
+              boost: {
+                value: 3
+              }
             }
+            
           }
         },
         {
           autocomplete:{
             query,
             path: "cast",
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
+            tokenOrder: "sequential",
+            score: {
+              boost: {
+                value: 3
+              }
             }
+            
           }
         },
         {
           autocomplete:{
             query,
             path: "directors",
-            fuzzy: {
-              maxEdits: 2,
-              prefixLength: 3
+            tokenOrder: "sequential",
+            score: {
+              boost: {
+                value: 2
+              }
             }
+            
           }
-        }      
-        
-        
-
+        }
       ],
       }
     }
-    const searchResults = await movieModel.aggregate().search(searchStage).limit(10).project("title plot -_id");
+    const searchResults = await movieModel
+      .aggregate()
+      .search(searchStage)
+      .limit(5)
+      .project("title type poster year imdb.rating -_id");
     return searchResults;
 }
 
