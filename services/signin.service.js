7a44bdd8be36aea8364c8b7f286eba895ecffe04
Amisha <amisha1324s@gmail.com>
2024-03-24 16:39:04 +0530

    updating user response

--- services/signin.service.js
+++ services/signin.service.js
@@ -1,34 +1,39 @@
 const usermodel = require("../models/user.model");
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 process.loadEnvFile();
-const exp_time=process.env.JWT_EXPIRATION_TIME;
-const sec_key =process.env.JWT_SECRET_KEY;
+const exp_time = process.env.JWT_EXPIRATION_TIME;
+const sec_key = process.env.JWT_SECRET_KEY;
 const httpStatus = require("http-status");
 
-const signinService =async(req,res)=>{
-    const{name, password}= req.body;
-    try{
-        const userexists = await usermodel.findOne({name:name});
-        if(!userexists){
-            return res.status(httpStatus.NOT_FOUND).json({alert: "User not found"});
+const signinService = async (req, res) => {
+    const { name, password } = req.body;
+    try {
+        const userexists = await usermodel.findOne({ name: name });
+        if (!userexists) {
+            return res.status(httpStatus.NOT_FOUND).json({ alert: "User not found" });
 
         }
         const chkpassword = await bcrypt.compare(password, userexists.password);
-        if(!chkpassword){
-            return res.status(httpStatus.BAD_REQUEST).json({message: "Wrong credentials"});
+        if (!chkpassword) {
+            return res.status(httpStatus.BAD_REQUEST).json({ message: "Wrong credentials" });
         }
 
 
 
-        
-        const token =jwt.sign({user: userexists}, sec_key);
-        res.status(httpStatus.OK).json({user:userexists, token: token});
 
-    } catch(error){
+        const token = jwt.sign({ user: userexists }, sec_key);
+        res.status(httpStatus.OK).json({
+            name: userexists.name,
+            emailid: userexists.emailid,
+            role: userexists.role,
+            subscriptionid: userexists.subscriptionid, token: token
+        });
+
+    } catch (error) {
         console.log(error);
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:"Error detected"});
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "Error detected" });
     }
-   
+
 }
-module.exports=signinService;
+module.exports = signinService;
--- services/signup.service.js
+++ services/signup.service.js
@@ -1,40 +1,46 @@
 const usermodel = require("../models/user.model");
-const jwt= require("jsonwebtoken");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 process.loadEnvFile();
-const exp_time=process.env.JWT_EXPIRATION_TIME;
-const sec_key =process.env.JWT_SECRET_KEY;
+const exp_time = process.env.JWT_EXPIRATION_TIME;
+const sec_key = process.env.JWT_SECRET_KEY;
 const httpStatus = require("http-status");
 
-const signupService = async(req,res) => {
-   
-    try{
+const signupService = async (req, res) => {
+
+    try {
         console.log(req.body);
-        const{name, password,emailid,role,subscriptionid}= req.body;
-        const userexists = await usermodel.findOne({name:name});
+        const { name, password, emailid, role, subscriptionid } = req.body;
+        const userexists = await usermodel.findOne({ name: name });
         console.log(userexists);
-        if(userexists){
-            return res.status(httpStatus.BAD_REQUEST).json({alert: "User already exists"});
+        if (userexists) {
+            return res.status(httpStatus.BAD_REQUEST).json({ alert: "User already exists" });
 
         }
 
-        const hpassword = await bcrypt.hash(password,8);
+        const hpassword = await bcrypt.hash(password, 8);
 
         const newuser = await usermodel.create({
             name: name,
             password: hpassword,
-            emailid:emailid,
+            emailid: emailid,
             role: role,
             subscriptionid: subscriptionid
         });
-        
-        const token =jwt.sign({user:newuser}, sec_key,{expiresIn: exp_time});
-        res.status(httpStatus.CREATED).json({user:newuser, token: token});
 
-    } catch(error){
-        console.log(error); 
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({message:"Error detected"});
+        const token = jwt.sign({ user: newuser }, sec_key, { expiresIn: exp_time });
+        res.status(httpStatus.CREATED).json({
+            name: newuser.name,
+            emailid: newuser.emailid,
+            role: newuser.role,
+            subscriptionid: newuser.subscriptionid,
+            token: token
+        });
 
+    } catch (error) {
+        console.log(error);
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).json({ message: "Error detected" });
+
+    }
 }
-}
-module.exports=signupService;
+module.exports = signupService;
